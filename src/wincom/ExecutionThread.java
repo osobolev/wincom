424a3d8ba93486d375ac728cb4fddcab020c1801
Oleg Sobolev <sobolev_ov@mail.ru>
2022-08-10 01:31:15 +0200

    Refactoing: use of ExecutorService instead of manual thread control

--- src/wincom/ExecutionThread.java
+++ src/wincom/ExecutionThread.java
@@ -3,156 +3,87 @@ package wincom;
 import com.jacob.com.ComThread;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.concurrent.*;
 
-public final class ExecutionThread implements Runnable {
+public final class ExecutionThread {
 
     private final ComLogger logger;
 
-    private final Object initLock = new Object();
-    private final Object queryWaitLock = new Object();
-    private final Object replyWaitLock = new Object();
+    private final ExecutorService service = Executors.newSingleThreadExecutor();
 
-    private final Thread thread = new Thread(this);
-
-    private Method method;
-    private Constructor<?> constructor;
-    private Object obj;
-    private Object[] args;
-    private Object result;
-    private Throwable error;
-
-    private boolean inited = false;
-    private boolean replyReady = false;
-    private boolean running = false;
+    private Boolean initOk = null;
+    private final Object lock = new Object();
 
     ExecutionThread(ComLogger logger) {
         this.logger = logger;
     }
 
-    private Object runSomethingInThread(Object[] args) throws ComException {
-        this.args = args;
-        this.result = null;
-        this.error = null;
-        synchronized (replyWaitLock) {
-            replyReady = false;
-            synchronized (queryWaitLock) {
-                if (!running)
-                    throw new IllegalStateException("COM object is not initialized properly");
-                queryWaitLock.notifyAll();
-            }
-            while (true) {
-                try {
-                    replyWaitLock.wait();
-                } catch (InterruptedException ex) {
-                    // ignore
-                }
-                if (replyReady)
-                    break;
-            }
-        }
-        if (error != null) {
-            throw new ComException(error);
-        }
-        return result;
-    }
-
-    public synchronized Object runMethodInThread(Method method, Object obj, Object[] args) throws ComException {
-        this.method = method;
-        this.constructor = null;
-        this.obj = obj;
-        return runSomethingInThread(args);
-    }
-
-    public synchronized <T> T runConstructorInThread(Constructor<T> constructor, Object[] args) throws ComException {
-        this.method = null;
-        this.constructor = constructor;
-        this.obj = null;
-        Class<T> cls = constructor.getDeclaringClass();
-        return cls.cast(runSomethingInThread(args));
-    }
-
-    public synchronized void createExecutionThread() {
-        synchronized (initLock) {
-            thread.start();
-            while (true) {
-                try {
-                    initLock.wait();
-                } catch (InterruptedException ex) {
-                    //
-                }
-                if (inited)
-                    break;
-            }
-        }
-    }
-
-    private void startFailed(Throwable th) {
-        logger.error(th);
-        synchronized (queryWaitLock) {
-            initFinished();
-        }
-    }
-
-    private void initFinished() {
-        synchronized (initLock) {
-            inited = true;
-            initLock.notifyAll();
-        }
-    }
-
-    public void run() {
+    private boolean runInternal(Runnable action) {
+        Future<?> future = service.submit(action);
         try {
-            ComThread.InitMTA();
-        } catch (Error | RuntimeException ex) {
-            startFailed(ex);
-            throw ex;
-        }
-        synchronized (queryWaitLock) {
-            running = true;
-            initFinished();
-            while (true) {
-                try {
-                    queryWaitLock.wait();
-                } catch (InterruptedException ex) {
-                    //
-                }
-                if (!running)
-                    break;
-                try {
-                    if (method != null) {
-                        result = method.invoke(obj, args);
-                    } else if (constructor != null) {
-                        result = constructor.newInstance(args);
-                    } else {
-                        // todo: add Dispatch.invoke/call
-                        result = null;
-                    }
-                } catch (InvocationTargetException itex) {
-                    error = itex.getTargetException();
-                } catch (Throwable ex) {
-                    error = ex;
-                } finally {
-                    synchronized (replyWaitLock) {
-                        replyReady = true;
-                        replyWaitLock.notifyAll();
-                    }
-                }
-            }
-        }
-        ComThread.Release();
-    }
-
-    public synchronized void release() {
-        synchronized (queryWaitLock) {
-            running = false;
-            queryWaitLock.notifyAll();
-        }
-        try {
-            thread.join();
+            future.get();
+            return true;
+        } catch (ExecutionException ex) {
+            logger.error(ex.getCause());
         } catch (InterruptedException ex) {
-            // ignore
+            Thread.currentThread().interrupt();
+        }
+        return false;
+    }
+
+    private <T> T runSomethingInThread(Callable<T> action) throws ComException {
+        synchronized (lock) {
+            if (initOk == null)
+                throw new IllegalStateException("Thread is not initialized: call createExecutionThread");
+            if (!initOk.booleanValue())
+                throw new IllegalStateException("COM object is not initialized properly");
+            Future<T> future = service.submit(action);
+            try {
+                return future.get();
+            } catch (ExecutionException ex) {
+                throw new ComException(ex.getCause());
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+                throw new ComException(ex);
+            }
+        }
+    }
+
+    public Object runMethodInThread(Method method, Object obj, Object[] args) throws ComException {
+        Callable<Object> action = () -> method.invoke(obj, args);
+        return runSomethingInThread(action);
+    }
+
+    public <T> T runConstructorInThread(Constructor<T> constructor, Object[] args) throws ComException {
+        Callable<T> action = () -> constructor.newInstance(args);
+        return runSomethingInThread(action);
+    }
+
+    public void createExecutionThread() {
+        synchronized (lock) {
+            if (initOk != null)
+                throw new IllegalStateException("Thread is already created");
+            initOk = runInternal(ComThread::InitMTA);
+        }
+    }
+
+    public void release() {
+        synchronized (lock) {
+            if (service.isShutdown())
+                throw new IllegalStateException("Thread is already released");
+            if (initOk != null && initOk.booleanValue()) {
+                runInternal(ComThread::Release);
+            }
+            service.shutdown();
+            try {
+                while (true) {
+                    if (service.awaitTermination(1, TimeUnit.HOURS))
+                        break;
+                }
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
         }
     }
 }
