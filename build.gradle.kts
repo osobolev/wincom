664fcaed463becdd6c70bbc43dd92c9f1d37ebd6
Oleg Sobolev <sobolev_ov@mail.ru>
2026-07-13 02:19:18 +0200

    Dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ description = "Windows COM wrappers"
 
 plugins {
     id("com.github.ben-manes.versions") version "0.54.0"
-    id("com.vanniktech.maven.publish") version "0.36.0"
+    id("com.vanniktech.maven.publish") version "0.37.0"
     `module-lib`
 }
 
